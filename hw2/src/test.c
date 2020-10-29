01c6889443f9a431d397c1330e47852eefad4986
imingasco <imingasco123@gmail.com>
2020-10-29 12:19:37 +0800

    error handling, modify float const

--- hw2/src/test.c
+++ hw2/src/test.c
@@ -1,7 +1,14 @@
-#include "stdio.h"
 int main()
 {
 /* 1: this is * just a / sample */
 int n = +1;
+float a = -1.0;
+float b = -1.;
+float c = 2.1e5;
+float d = -2.1E6;
+float e = 2.e-2;
+float f = 2.E-3;
+float m = 3.;
+float ab = 2.;
 printf("%d\n", n);
 }
